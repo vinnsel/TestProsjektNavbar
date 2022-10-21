f2c9be9eed1d44d12878a455a1b556c2ddd55299
vinnsel <vinnsel@hotmail.com>
2022-10-21 17:42:03 +0200

    storeArray

--- array.js
+++ array.js
@@ -1,23 +1,48 @@
-const inputPris = document.getElementById("inputPris")
+const formPris = document.getElementById("inputPris")
+const formArtikkel = document.getElementById("inputArtikkel")
 const inputBtn = document.getElementById("inputBtn")
 const showInput = document.getElementById('results')
+const resultList = document.getElementById("result-list")
+const storage = window.localStorage // opprett egen variabel med tilgag til localStorage
+const displayArray = JSON.parse(storage.getItem("prisArray")) // Hent informasjon fra localStorage og parse til JS fra JSON
+const prisArray = []
 
-const pris = []
-
+ // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 function input() {
 
-    pris.push(inputPris.value)
-//    console.log(inputPris.value)
-     
-//    console.log(showInput.innerText = pris)
+    const inputObject = {
+      artikkel: formArtikkel.value,
+      pris: formPris.value
+    };
+ 
+    prisArray.push(inputObject) // dytt ny verdi inn i prisArray
+    const storageArray = JSON.stringify(prisArray) // opprett en ny variabel med en JSON-verdi med prisArray som kilde
+    storage.setItem("prisArray", storageArray) // lagre "prisArray" (key) med storageArray (value) i localStorage
+
+    // Akkurat nå overskrives verdiene i localStorage hver gang input-funksjonen kjøres (når knappen trykkes)
+    // Mål: hente verdiene som ligger i localStorage, legg til nye verdier/oppdater "gammel" array, oppdater/push nye verdier inn i localStorage
+
+    // resultList.innerHTML = ''
     
-    for (let i=0; i < pris.length; i++) {
-            let prisArray = showInput.innerText = pris
-        }
-        
+    console.log(prisArray)
+    // resultList.innerHTML = resultItems
+
+
 }
 
-  inputBtn.addEventListener("click", input)
+// Når siden laster første gang kjøres displayArray.map - denne bør inn i en egen funksjon som kjøres både når siden laster første gang (function call) og når noen trykker på knappen
+displayArray.map((item) => {
+  const li = document.createElement('li') // opprett et <li> element
+  //local storage
+  const textOutput = 'Artikkelnavn: ' + item.artikkel + ', er priset til ' + item.pris + ',-' 
+  
+  li.appendChild(document.createTextNode(textOutput)) // legg verdien av textOutput inn som textNode-barn av <li> elementet
+  resultList.appendChild(li) // legg <li> elementet inn som et node-barn av <ul> elementet
+
+})
+
+
+inputBtn.addEventListener("click", input)
 
 /*
   const prisTest = ['t1', 't2', 't3', 't4']
@@ -42,13 +67,22 @@ function input() {
 
 
 
+/*
+const enkeltObjekt = {
+  produktnavn: "",
+  kostpris: 0,
 
 
+}
 
 
+let arr = [];
 
+const obj = {name: 'Tom'};
 
+arr.push(obj);
 
+console.log(arr); // 👉️ [{name: 'Tom'}]
 
-
+*/
 
