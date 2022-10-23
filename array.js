dba0b246d06ffb8eb184f0ed8999984ac0c3b8ab
vinnsel <73517111+vinnsel@users.noreply.github.com>
2022-10-23 12:21:43 +0200

    array

--- array.js
+++ array.js
@@ -6,7 +6,7 @@ const resultList = document.getElementById("result-list")
 const storage = window.localStorage // opprett egen variabel med tilgag til localStorage
 const displayArray = JSON.parse(storage.getItem("prisArray")) // Hent informasjon fra localStorage og parse til JS fra JSON
 
-
+console.log(showInput)
 
 const prisArray = []
 
@@ -26,32 +26,42 @@ function input() {
 
     const storageArray = JSON.stringify(prisArray) // opprett en ny variabel med en JSON-verdi med prisArray som kilde
     storage.setItem("prisArray", storageArray) // lagre "prisArray" (key) med storageArray (value) i localStorage
-    console.log('storageArray')
-    console.log(storageArray)    
+  
+       
     // Akkurat nå overskrives verdiene i localStorage hver gang input-funksjonen kjøres (når knappen trykkes)
     // Mål: hente verdiene som ligger i localStorage, legg til nye verdier/oppdater "gammel" array, oppdater/push nye verdier inn i localStorage
 
     // resultList.innerHTML = ''
-    
-    console.log(prisArray)
+    console.log({storageArray}) 
+    console.log({prisArray})
     // resultList.innerHTML = resultItems
 
+    for (let i=0; i < prisArray.length; i++) {
+  
+  //const li = document.createElement('li') // opprett et <li> element
+  //local storage
+  console.log(prisArray[i].artikkel)
+  const textOutput = 'Artikkelnavn: ' + prisArray[i].artikkel + ', er priset til ' + prisArray[i].pris + ',-' 
+  showInput.innerText = textOutput
+  
+ // li.appendChild(document.createTextNode(textOutput)) // legg verdien av textOutput inn som textNode-barn av <li> elementet
+ // resultList.appendChild(li) // legg <li> elementet inn som et node-barn av <ul> elementet
+
+ for (let i=0; i < storageArray.length; i++) {
+
+  console.log(storageArray[i].pris)
+  showInput.innerText = storageArray[i].pris
+}
 
 }
+}
+
   function storeArrayfunction() {
   // Når siden laster første gang kjøres displayArray.map - denne bør inn i en egen funksjon som kjøres både når siden laster første gang (function call) og når noen trykker på knappen
-  displayArray.map((item) => {
-  const li = document.createElement('li') // opprett et <li> element
-  //local storage
-  const textOutput = 'Artikkelnavn: ' + item.artikkel + ', er priset til ' + item.pris + ',-' 
-  console.log(textOutput)
-  li.appendChild(document.createTextNode(textOutput)) // legg verdien av textOutput inn som textNode-barn av <li> elementet
-  resultList.appendChild(li) // legg <li> elementet inn som et node-barn av <ul> elementet
 
-})
 }
-console.log(localStorage)
-showInput.innerText = 'test'
+//console.log(localStorage)
+//showInput.innerText = 'test'
 
 inputBtn.addEventListener("click", input)
 
@@ -62,15 +72,16 @@ inputBtn.addEventListener("click", input)
     console.log(prisTest[i])
 
     function kalkyle(prisTest1) {
-        showInput.innerText = prisTest1
-        console.log(prisTest1)
+        prisTestSplit = prisTest1.join('\r\n')
+        showInput.innerText = prisTestSplit
+        console.log(prisTestSplit)
     }
     kalkyle(prisTest)
     
 
 
 }
-*/
+
 
 //  let arrayTest = showInput.innerText = prisTest
 //  console.log(prisTest)
@@ -78,7 +89,7 @@ inputBtn.addEventListener("click", input)
 
 
 
-/*
+
 const enkeltObjekt = {
   produktnavn: "",
   kostpris: 0,
