5f10f99ea1678a93fd189ee91bc49a614501c372
vinnsel <73517111+vinnsel@users.noreply.github.com>
2022-10-24 20:36:25 +0200

    Update array.js

--- array.js
+++ array.js
@@ -5,58 +5,56 @@ const showInput = document.getElementById('results')
 const resultList = document.getElementById("result-list")
 const storage = window.localStorage // opprett egen variabel med tilgag til localStorage
 const displayArray = JSON.parse(storage.getItem("storeTilbudArray")) // Hent informasjon fra localStorage og parse til JS fra JSON
-
-
 const displayTilbudArray = JSON.parse(localStorage.getItem("storeTilbudArray")) 
-console.log(displayTilbudArray)
-
-const tilbudArray = []
-
-
  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 
-function input() {
+function input(tilbudArray) {
 
-    const inputObject = {
-      artikkel: formArtikkel.value,
-      pris: formPris.value
-    }
+  const inputObject = {
+    artikkel: formArtikkel.value,
+    pris: formPris.value
+  }
 
-    tilbudArray.push(inputObject) // dytt ny verdi inn i prisArray
+  tilbudArray.push(inputObject) // dytt ny verdi inn i prisArray
 
-    //skriver vedier til DOM
-    const visVarePris = tilbudArray.map(element => `Artikkelnavn: ${element.artikkel} Pris: ${element.pris},- `)
-    showInput.innerText = visVarePris.join('\r\n')
-  
-  
+  //skriver vedier til DOM
+  const visVarePris = tilbudArray.map(element => `Artikkelnavn: ${element.artikkel} Pris: ${element.pris},- `)
+  showInput.innerText = visVarePris.join('\r\n')
 
-    const storageArray = JSON.stringify(tilbudArray) // opprett en ny variabel med en JSON-verdi med prisArray som kilde
-    storage.setItem("storeTilbudArray", storageArray) // lagre "prisArray" (key) med storageArray (value) i localStorage
-    console.log(storageArray)
 
-       
-    // Akkurat nå overskrives verdiene i localStorage hver gang input-funksjonen kjøres (når knappen trykkes)
-    // Mål: hente verdiene som ligger i localStorage, legg til nye verdier/oppdater "gammel" array, oppdater/push nye verdier inn i localStorage
 
-    console.log({storageArray}) 
-    console.log({tilbudArray})
+  const storageArray = JSON.stringify(tilbudArray) // opprett en ny variabel med en JSON-verdi med prisArray som kilde
+  storage.setItem("storeTilbudArray", storageArray) // lagre "prisArray" (key) med storageArray (value) i localStorage
+  console.log(storageArray)
 
-  
+     
+  // Akkurat nå overskrives verdiene i localStorage hver gang input-funksjonen kjøres (når knappen trykkes)
+  // Mål: hente verdiene som ligger i localStorage, legg til nye verdier/oppdater "gammel" array, oppdater/push nye verdier inn i localStorage
+
+  console.log({storageArray}) 
+  console.log({tilbudArray})
 }
 
 
 
+  if (displayTilbudArray == null) {
+    const tilbudArray = []
+    console.log(tilbudArray)
+    console.log(true)
+    inputBtn.addEventListener("click", input(tilbudArray))
+    }
+
+  else {
+    const tilbudArray = displayTilbudArray
+    console.log(tilbudArray)  
+    console.log(false)
+    inputBtn.addEventListener("click", input(tilbudArray))
+    }
 
 
 
-//  function storeArrayfunction() {
-  // Når siden laster første gang kjøres displayArray.map - denne bør inn i en egen funksjon som kjøres både når siden laster første gang (function call) og når noen trykker på knappen
 
-//}
-//console.log(localStorage)
-//showInput.innerText = 'test'
 
-inputBtn.addEventListener("click", input)
 
 
 
