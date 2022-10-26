ab890f907012cdd1d2d833cbd8ae6a95be59f90d
vinnsel <73517111+vinnsel@users.noreply.github.com>
2022-10-26 20:45:51 +0200

    Update array2.js

--- array2.js
+++ array2.js
@@ -3,12 +3,8 @@ const inputArtikkel = document.getElementById("inputArtikkel")
 const inputBtn = document.getElementById("inputBtn")
 const showInput = document.getElementById('results')
 const storage = window.localStorage // opprett egen variabel med tilgag til localStorage
- const displayArray = JSON.parse(storage.getItem("storeTilbudArray")) // Hent informasjon fra localStorage og parse til JS fra JSON
-// const displayTilbudArray = JSON.parse(localStorage.getItem("storeTilbudArray")) 
+const displayArray = JSON.parse(storage.getItem("storeTilbudArray")) // Hent informasjon fra localStorage og parse til JS fra JSON
 
- // const displayArray = []
-
-console.log(displayArray)
 
 function lagreInputArray() {
 
@@ -16,32 +12,45 @@ function lagreInputArray() {
           artikkel: inputArtikkel.value,
           pris: inputPris.value
         } 
+
+      if (displayArray == null)  {
+        const displayArray = []
         displayArray.push(inputObject) // dytt ny verdi inn i prisArray
-        
-    
         const storageArray = JSON.stringify(displayArray) // opprett en ny variabel med en JSON-verdi med prisArray som kilde
         storage.setItem("storeTilbudArray", storageArray) // lagre "prisArray" (key) med storageArray (value) i localStorage
         console.log(displayArray)
-
-
         visInputArray()
+      
+      }
+      else {
+        displayArray.push(inputObject) // dytt ny verdi inn i prisArray
+        const storageArray = JSON.stringify(displayArray) // opprett en ny variabel med en JSON-verdi med prisArray som kilde
+        storage.setItem("storeTilbudArray", storageArray) // lagre "prisArray" (key) med storageArray (value) i localStorage
+        console.log(displayArray)
+        
+      }
+visInputArray()
+        
 
       } 
       
 
     function visInputArray() {
-      // if (erd det noe i localStorage?) {
-      //   ja det er det
-      // } else {
-      //   nei det var det visst ikke
-      // }
+      
+      if (!displayArray) {
+        showInput.innerText = `Ingen resultat`
+        console.log('nei')
+      }
+      else {
+        console.log('JA')
       const hentFraLocalStorge = JSON.parse(storage.getItem("storeTilbudArray"))
       const varePris = hentFraLocalStorge.map(element => `Artikkelnavn: ${element.artikkel} Pris: ${element.pris},- `);
       showInput.innerText = varePris.join('\r\n')
     }
-
+}
     visInputArray()
 
 
 
-  inputBtn.addEventListener("click", lagreInputArray)
+
+   inputBtn.addEventListener("click", lagreInputArray)
